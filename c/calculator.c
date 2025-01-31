5ecd2a113ae1efe58b4c6ee384acb4db495ad7d7
Lazres31 <lazres31@yandex.ru>
2025-01-31 21:06:38 +0300

    Add remainder and int division support

--- c/calculator.c
+++ c/calculator.c
@@ -5,14 +5,17 @@
 #define MINUS_NUM 2
 #define DIVIDE_NUM 3
 #define MULTIPLE_NUM 4
-
+#define INT_DIVIDE_NUM 5
+#define REMAINDER 6
 int main(void) {
 	long double a,op,b;
 	printf("Enter first number: ");
 	scanf("%Lf", &a);
 	printf("Choose operation: \n\t[%d]: +"
-			"\n\t[%d]: -\n\t[%d]: //\n\t[%d]: *\n",
-			PLUS_NUM, MINUS_NUM, DIVIDE_NUM, MULTIPLE_NUM );
+			"\n\t[%d]: -\n\t[%d]: /\n\t[%d]: *"
+			"\n\t[%d]: //\n\t[%d]: %%\n\t",
+			PLUS_NUM, MINUS_NUM, DIVIDE_NUM,
+			MULTIPLE_NUM, INT_DIVIDE_NUM, REMAINDER );
 	scanf("%Lf", &op);
 	printf("Enter second number: ");
 	scanf("%Lf", &b);
@@ -26,6 +29,10 @@ int main(void) {
 		printf("%Lf\n", a/b);
 	else if (op == MULTIPLE_NUM)
 		printf("%Lf\n", a*b);
+	else if (op == INT_DIVIDE_NUM)
+		printf("%lld\n", (long long int)a/(long long int)b);
+	else if (op == REMAINDER)
+		printf("%lld\n", (long long int)a%(long long int)b);
 	else
 		printf("Undefined operation\n");
 
