7e33308721b4b0ec772ae49cb49d25a04c90d642
Lazres31 <lazres31@yandex.ru>
2025-02-01 13:38:07 +0300

    Move calculate logic to a function

--- c/calculator.c
+++ c/calculator.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include "calculator.h"
 
 int main(void) {
 	char op[2] = {' '};
@@ -7,22 +9,28 @@ int main(void) {
 	printf("Available operations: +,-,/,*,//,%%\n"
 			"Enter first number then operation and then second number: ");
 	scanf(" %Lf %s %Lf", &a, op, &b);
+	
+	printf("Result: %Lf\n", calculate(a, op, b));
+	return EXIT_SUCCESS;
+}
 
-	printf("Result: ");
+long double calculate(long double a, char* op, long double b) {
 	if (op[0] == '+')
-		printf("%Lf\n", a+b);
+		return a + b;
 	else if (op[0] == '-')
-		printf("%Lf\n", a-b);
+		return a - b;
 	else if (op[0] == '*')
-		printf("%Lf\n", a*b);
+		return a * b;
 	else if (op[0] == '/' && op[1] == '/')
-		printf("%lld\n", (long long int)a/(long long int)b);
+		return (long double)( (long long int)a / (long long int)b );
 	else if (op[0] == '/')
-		printf("%Lf\n", a/b);
+		return a / b;
 	else if (op[0] == '%')
-		printf("%lld\n", (long long int)a%(long long int)b);
-	else
-		printf("Undefined operation\n");
-
-	return 0;
+		return (long double)( (long long int)a % (long long int)b );
+	else {
+		printf("Operation: \"%s\" is not defined\n", op);
+		exit(EXIT_FAILURE);
+	}
+	puts("[calculate]: Reached unreachable code. Send angry email to developer");
+	exit(EXIT_FAILURE);
 }
---
+++ c/calculator.h
@@ -0,0 +1,6 @@
+#ifndef CALCULATOR
+#define CALCULATOR
+
+long double calculate(long double a, char* op, long double b);
+
+#endif
