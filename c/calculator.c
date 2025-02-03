56adbc6d84d0d789ec82214504b0c20adc041bd5
Lazres31 <lazres31@yandex.ru>
2025-02-03 11:41:40 +0300

    Some safe implementations and cleanup

--- c/calculator.c
+++ c/calculator.c
@@ -9,17 +9,23 @@
 int main(int argc, char** argv) {
 	char s[MAX_VALUE_OF_CHARACTERS] = {' '};
 	char op[2] = {' '};
-	char* ptr_s_subarr;
+	char* ptr_s_subarr = NULL;
 	long double a,b;
 	
 	if (argc <= 1) {
 		printf("Available operations: +,-,/,*,//,%%\n"
-			"Enter first number then operation and then second number: ");
-		fgets(s, sizeof(s), stdin);
+			"Enter first number, then operation"
+			"and then second number: ");
+		char* fgets_status = NULL;
+		fgets_status = fgets(s, sizeof(s)/sizeof(s[0]), stdin);
+		if (fgets_status == NULL) {
+			puts("There's no meaningfull symbols in the string");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else {
-		//Parse characters from 2 dimension argv array to
-		//one dimension s array
+		//Parse characters from 2 dimensional argv array to
+		//one dimensional s array
 		int iterations_sum = 0;
 		for (int i = 1; i < argc; i++) {
 			for (int j = 0; argv[i][j] != '\0'; j++) {
@@ -40,28 +46,6 @@ int main(int argc, char** argv) {
 	return EXIT_SUCCESS;
 }
 
-long double calculate(long double a, char* op, long double b) {
-	if (op[0] == '+')
-		return a + b;
-	else if (op[0] == '-')
-		return a - b;
-	else if (op[0] == '*')
-		return a * b;
-	else if (op[0] == '/' && op[1] == '/')
-		return (long double)( (long long int)a / (long long int)b );
-	else if (op[0] == '/')
-		return a / b;
-	else if (op[0] == '%')
-		return (long double)( (long long int)a % (long long int)b );
-	else {
-		printf("Operation: \"%s\" is not defined\n", op);
-		exit(EXIT_FAILURE);
-	}
-	puts(	"[calculate]: Reached unreachable code."
-			"Send angry email to the dev.");
-	exit(EXIT_FAILURE);
-}
-
 void parse_operator(char** ps, char* op, int op_arr_len) {
 	char op_symbols[] = ACCEPTABLE_OPERATORS;
 	char* s = *ps;
@@ -79,10 +63,7 @@ void parse_operator(char** ps, char* op, int op_arr_len) {
 			op_len++;
 
 		} else if (op_len != 0) {
-			if (i != MAX_VALUE_OF_CHARACTERS)
-				*ps = &s[i];
-			else
-				*ps = NULL;
+			*ps = &s[i];
 			return;
 		}
 
@@ -90,4 +71,25 @@ void parse_operator(char** ps, char* op, int op_arr_len) {
 
 	puts("There's no operator in the input");
 	exit(EXIT_FAILURE);
+}
+
+long double calculate(long double a, char* op, long double b) {
+	if (a == 0) puts("WARNING: a = 0");
+	if (b == 0) puts("WARNING: b = 0");
+	if (op[0] == '+')
+		return a + b;
+	else if (op[0] == '-')
+		return a - b;
+	else if (op[0] == '*')
+		return a * b;
+	else if (op[0] == '/' && op[1] == '/')
+		return (long double)( (long long int)a / (long long int)b );
+	else if (op[0] == '/')
+		return a / b;
+	else if (op[0] == '%')
+		return (long double)( (long long int)a % (long long int)b );
+	else {
+		printf("Operation: \"%s\" is not defined\n", op);
+		exit(EXIT_FAILURE);
+	}
 }
